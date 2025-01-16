4aad720eb5d0e313525b9b2ee4af2325c2acf37b
오세훈 <155278072+xlxhollywood@users.noreply.github.com>
2025-01-16 18:29:59 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
@@ -14,7 +14,6 @@ int main() {
             a[cnt++] = i; // 배열에 i 값 할당 후 cnt 증가
             if (cnt == 1001) break; // cnt가 a[A] <= a[B] <= a[1000] 이므로 1000에서 정지 
         }
-        if (cnt == 1001) break;
     }
 
   
