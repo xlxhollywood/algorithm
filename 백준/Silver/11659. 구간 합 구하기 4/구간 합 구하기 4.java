e7be12441125db6a9c613b7830c58b2ca5c7897a
오세훈 <155278072+xlxhollywood@users.noreply.github.com>
2025-01-19 21:04:03 +0900

    Update 구간 합 구하기 4.java

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Main {
 
-        // 입력 받기
-        int n = sc.nextInt(); // 수의 개수
-        int m = sc.nextInt(); // 합을 구해야 하는 횟수
-        int[] numbers = new int[n + 1]; // 1-based index 사용
-        int[] prefixSum = new int[n + 1];
+  public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      
+      
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      // 정적 배열 선언 
+      int [] numbers = new int[n+1];
+      int [] pSum = new int[n+1];
 
-        // 수 입력
-        for (int i = 1; i <= n; i++) {
-            numbers[i] = sc.nextInt();
-            prefixSum[i] = prefixSum[i - 1] + numbers[i]; // 누적합 계산
-        }
+      // 구간 합 배열 만들기
+      for(int i = 1 ; i <= n; i++){
+      // ex) 1, 2, 3, 4, 5
+      numbers[i] = sc.nextInt();
+      // pSum[1] = 0 + 1 , pSum[2] = 1 + 2,  pSum[3] = 3 + 3
+      pSum[i] = pSum[i-1] + numbers[i];
+      }
 
-        // 구간합 계산
-        StringBuilder sb = new StringBuilder();
-        for (int k = 0; k < m; k++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            sb.append(prefixSum[j] - prefixSum[i - 1]).append("\n");
-        }
+      for (int k = 0; k < m; k++){
+      int i = sc.nextInt();
+      int j = sc.nextInt();
+      // 2,4 입력시 , pSum[4] - pSum[1] 
+      // pSum[2]가 아닌 이유는 2 이전에 있는 배열의 원소들을 빼야 되기 때문이다
+      // ex) 0, 1, 2, 3, 4, 5
+      //          <------->
+      //           2 + 3 + 4 = 9
+      // ex) 0, 1, 3, 6, 10, 15
+      //     <------------>
+      //     <--->         
+      //           10 - 1 = 9
+      int sum = pSum[j] - pSum[i-1];
+      System.out.println(sum);
+      }
 
-        // 결과 출력
-        System.out.print(sb.toString());
-        sc.close();
-    }
+  
+
+      
+      
+      
+  }
+  
 }
