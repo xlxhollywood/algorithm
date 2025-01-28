55fa08dba1085fd15c82208698e6f1b04ab812e8
오세훈 <155278072+xlxhollywood@users.noreply.github.com>
2025-01-29 03:49:15 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 200 ms, Memory: 18140 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,38 +1,50 @@
 import java.util.*;
 
+// sc.nextLine() : 띄어쓰기까지 받기
+// sc.next() : 토큰 단위
+// br.readLine() : 띄어쓰기까지 받기
 class Main {
-  public static void main(String [] args){
-
+  public static void main(String [] args) {
+    /*
+      1. 의상의 테스트 케이스 입력 받기.
+      2. 의상의 개수 입력 받기
+      3. 자료구조 Map 선언
+      4. Map에 의상 종류를 key로 종류의 개수를 value로 입력 받기
+      5. 경우의 수 계산하기
+     */
     Scanner sc = new Scanner(System.in);
 
     // 테스트 케이스 입력
-    int T = sc.nextInt();
+    int caseCount = sc.nextInt();
 
-    for(int tc = 1; tc <= T; tc++){
+
+    for(int i = 0; i < caseCount; i++){
       // 의상의 종류 개수 입력
       int n = sc.nextInt();
 
-      //의상 종류별 개수를 저장할 Map 생성
       Map<String,Integer> map = new HashMap<>();
-
-      // 의상 이름과 종류 입력 및 Map에 저장
-      for(int i = 0; i < n; i++){
+    
+      for(int j = 0; j < n; j++){
         String name = sc.next(); // 의상 이름
         String type = sc.next(); // 의상 종류
-        int count = map.getOrDefault(type, 0); // 기존 값 가져오기
-        map.put(type, count + 1); // 업데이트된 값 저장
+        int count = map.getOrDefault(type, 0); // 기존 값 가져오기, 없다면 0
+        map.put(type, count + 1); // 없는 경우 0 + 1, 있는 경우 n + 1이 된다.
       }
 
-      // 가능한 모든 경우의 수를 계산한다.
-      int ans = 1;
-      for(int val : map.values()){
-        ans *= val + 1;
+      // 경우의 수 계산
+      int answer = 1;
+      for(int value : map.values()){
+        // 3C1 = 3. 2C1 = 2 --> 3C1 × 2C1 = 3 × 2 = 6
+        // [headgear] : hat, turban, NULL = 2 + 1
+        // [eyewear] : sunglasses, NULL = 1 + 1
+        answer *= (value + 1);
       }
 
-      // 모두 다 벗는 경우의 수는 제이
-      System.out.println(ans - 1); 
+      // 모두 벗은 경우의 수는 제외 할 것.
+      System.out.println(answer - 1);
 
-    }
+  }
+
   }
 
 }
