78fe58ecc0a76a0d54d0cfd64f6f8fcec792eb04
오세훈 <155278072+xlxhollywood@users.noreply.github.com>
2025-02-07 22:12:53 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -4,18 +4,15 @@ import java.io.*;
 class Main {
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
-    
     Set<Integer> set = new HashSet<>();    
-    int cnt = 0;
-    int flag = 0;
+
     for(int i = 0; i < 10; i++){
         int n = Integer.parseInt(br.readLine());
         set.add(n % 42);
     }    
-  
-// 39 40 41 0 1 2 40 41 0 1 = 6개 ?? 중복만 없으면 되는거 아님 ?? 
 
     System.out.println(set.size());
+
   }
 
 }
